2ffc46ea7b7448b41b5a515325c8f21b344bfdec
VictoriaBihan <biganviktoria@gmail.com>
2024-09-17 13:46:15 +0300

    jumping

--- Assets/script/Player.cs
+++ Assets/script/Player.cs
@@ -4,32 +4,65 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    SpriteRenderer rend;
     public float movementSpeed = 1f;
-
+    public float jumpForce = 2f;
+    SpriteRenderer rend;
+    Animator anim;
+    public float timer;
+    Rigidbody2D rb;
 
 
     // Start is called before the first frame update
     void Start()
     {
       Debug.Log("Start");
-      rend=GetComponent<SpriteRenderer>() ;
+      rend=GetComponent<SpriteRenderer>();
+      anim=GetComponent<Animator>();
+      rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+        // Update is called once per frame
     void Update()
     {
-       if(Input.GetKey(KeyCode.A))
-       {
+      anim.SetBool("moving", false);
+      if(Input.GetKey(KeyCode.A))
+      {
         transform.Translate(Vector2.left * movementSpeed * Time.deltaTime, Space.World);
         rend.flipX = true;
-
-       } 
-       if(Input.GetKey(KeyCode.D))
-       {
+        anim.SetBool("moving", true);
+      } 
+      if(Input.GetKey(KeyCode.D))
+      {
         transform.Translate(Vector2.right * movementSpeed * Time.deltaTime, Space.World);
         rend.flipX = false;
+        anim.SetBool("moving", true); 
+      }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+            timer = 0;
 
-       } 
+        }
+        else
+        {
+            timer += Time.deltaTime;
+        }
+        anim.SetFloat("Dance_timer", timer);
+
+        bool grounded = false;
+        LayerMask LayerMask = LayerMask.GetMask("Ground");
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.6f, LayerMask);
+        Debug.DrawRay(transform.position, Vector2.down * 0.6f, Color.yellow);
+
+        if(hit)
+        {
+            grounded = true;
+            Debug.Log("Ground found");
+        }
+
+        if(Input.GetKeyDown(KeyCode.W) && grounded)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+
     }
 }
